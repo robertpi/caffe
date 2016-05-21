c719df2b244952298bca34ea74c140dee42b2e17
Robert Pickering <robert@strangelights.com>
2016-05-21 10:45:36 +0200

    better structuring and comments for the example

--- windows/caffe.clr/Common.fs
+++ windows/caffe.clr/Common.fs
@@ -3,3 +3,11 @@
 type Phase =
     | Train = 0
     | Test = 1
+
+// the shape of a layer or blob, can't be an emun
+// as we can have an arbitrary number of dimensions
+module Shape =
+    let Num = 0
+    let Channels = 1
+    let Width = 2
+    let Height = 3
--- windows/examples/fs_classification/Program.fs
+++ windows/examples/fs_classification/Program.fs
@@ -7,79 +7,93 @@ module Classification =
     open System.Runtime.InteropServices
     open Caffe.Clr
 
-    let Shape_Num = 0
-    let Shape_Channels = 1
-    let Shape_Width = 2
-    let Shape_Height = 3
-
     let CV_32FC3 = 21
 
     [<EntryPoint>]
     let main argv = 
+        // unpack arguments related to the model
         let modelFile   = argv.[0]
         let trainedFile = argv.[1]
         let meanFile    = argv.[2]
         let labelFile   = argv.[3]
 
-        let file = argv.[4];
+        // unpack the argument for the files to be tested
+        let file = argv.[4]
 
+        // load the network and training data
         let net = new Net(modelFile, Phase.Test)
         net.CopyTrainedLayersFrom(trainedFile)
 
+        // check the network has the right number of inputs/outputs
         assert (net.InputBlobs.Count = 1)
         assert (net.OutputBlobs.Count = 1)
 
+        // grab a reference to the input blob where will put the image
         let inputBlob = net.InputBlobs |> Seq.head
-        
-        let meanBlob = Blob.FromProtoFile(meanFile)
 
-        assert (meanBlob.Shape(Shape_Channels) = inputBlob.Shape(Shape_Channels))
-
-        let size = new Size(inputBlob.Shape(Shape_Width), inputBlob.Shape(Shape_Height))
-        
+        let size = new Size(inputBlob.Shape(Shape.Width), inputBlob.Shape(Shape.Height))
 
         let img = OpenCV.ImageRead(file, -1)
 
-        // TODO don't resize if we're the same size
-        let imgResized = OpenCV.Resize(img, size.Width, size.Height)
+        // resize the image if necessary
+        let imgResized =
+            if img.Width = size.Width && img.Height = size.Height then 
+                img
+            else
+                OpenCV.Resize(img, size.Width, size.Height)
 
         let sampleFloat = imgResized.ConvertTo(CV_32FC3)
 
         let numChannels = 3 
 
+        // load the mean blob, calculated from the training data
+        let meanBlob = Blob.FromProtoFile(meanFile)
+
+        // check the mean blob has some number of channels as the input blob
+        assert (meanBlob.Shape(Shape.Channels) = inputBlob.Shape(Shape.Channels))
+
+        // calculate an overall mean from the mean blob and create a matrix from this
         let mean = meanBlob.GetData()
-        let meanMatrix = OpenCV.MergeFloatArray(mean, numChannels, meanBlob.Shape(Shape_Width), meanBlob.Shape(Shape_Height))
+        let meanMatrix = OpenCV.MergeFloatArray(mean, numChannels, meanBlob.Shape(Shape.Width), meanBlob.Shape(Shape.Height))
         let mean = OpenCV.Mean(meanMatrix)
         let meanMatrix = Matrix.FromDimensions(size.Width, size.Height, meanMatrix.Type(), mean)
 
-
+        // subtract the mean matrix from the sample float
         let sampleNormalized = OpenCV.Subtract(sampleFloat, meanMatrix)
-
-        OpenCV.SplitToInputBlob(sampleNormalized, inputBlob)
-
+    
+        // reshapre the input blob and network to suite out input image
         inputBlob.Reshape([|1; numChannels; size.Width; size.Height|])
         net.Reshape()
 
+        // load normalized values into the input blob
+        OpenCV.SplitToInputBlob(sampleNormalized, inputBlob)
 
+        // forward the data though the network
         let loss = ref 0.0f
         net.Forward(loss)
 
-
+        // grab a reference to the output blob
         let output = net.OutputBlobs |> Seq.head
 
+        // read the labels file
         let labels = File.ReadAllLines(labelFile)
-        let resultChannels = output.Shape(Shape_Channels)
 
+        // check the network has the same number of outputs as the labels file
+        let resultChannels = output.Shape(Shape.Channels)
         assert (labels.Length = resultChannels)
 
+        // get the output data
         let resultData = output.GetData()
 
+        // match the results to labels and sort
         let results =
             Seq.zip resultData labels
             |> Seq.sortByDescending fst
             |> Seq.take 10
 
+        // print the top 10 results
         for (percent, label) in results do
             printfn "%f %s" percent label
 
+        // exit zero
         0
